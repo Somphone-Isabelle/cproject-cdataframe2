0d510b51b5a8c90a064ef9258a72d9fdc333c35c
Somphone-Isabelle <isabelle.somphone@efrei.net>
2024-04-18 19:22:12 +0200

    Archi CDF

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # cproject-cdataframe2
+
+For windows user:
+    Install Cygwin64 Terminal in order to compile and execute the code.
+        To compile it:
+            -$ gcc *.c -o main.exe
+        To execute it:
+            -$ ./main.exe
---
+++ cdataframe.h
@@ -0,0 +1,39 @@
+#ifdef CDATAFRAME_H
+#define CDATAFRA%E_H
+#include "column.h"
+
+typedef struct{
+    COLUMN** columns;
+    int nbcol;
+    int maxcol;
+}CDATAFRAME;
+
+//FILLING FUNCTIONS
+CDATAFRAME* create_cdf();
+void user_imput(CDATAFRAME* dataf);
+void hardfill_df(CDATAFRAME* dataf);
+
+//DISPLAYING FUNCTIONS
+void display_df(CDATAFRAME* dataf);
+void disp_part_row(CDATAFRAME* dataf, int start, int end);
+void disp_part_col(CDATAFRAME* dataf, int start, int end);
+
+//OPERATIONS FUNCTIONS
+void add_row(CDATAFRAME* dataf, int* vals);
+void del_row(CDATAFRAME* dataf, int i); //i = index
+void add_col(CDATAFRAME* dataf, COLUMN* column);
+void del_col(CDATAFRAME* dataf, int i);
+void rename_col(CDATAFRAME* dataf, int j, char* new_title);
+int search_val(CDATAFRAME* dataf, int val);
+int get_cell(CDATAFRAME* dataf, int i, int j);
+void replace_val(CDATAFRAME* dataf,int i, int j)
+void disp_col_names(CDATAFRAME* dataf);
+
+//ANALYSIS FUNCTIONS
+int display_nbrows(CDATAFRAME* dataf);
+int display_nbcols(CDATAFRAME* dataf);
+int disp_cell_equal(CDATAFRAME* dataf, int x);
+int disp_cell_great(CDATAFRAME* dataf, int x);
+int disp_cell_lower(CDATAFRAME* dataf, int x);
+
+#enfif 
